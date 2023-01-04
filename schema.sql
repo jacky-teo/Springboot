db53dd11cabcb175c4db8c9ff6abfadfd735aacd
jacky-teo <jackytjq@outlook.com>
2023-01-04 10:25:02 +0800

    Added SQL create file

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Jobs (
+    id int,
+    job_title varchar(255),
+    job_description varchar(255),
+    job_responsibility varchar(255),
+    requirements varchar(255),
+    job_type varchar(255),
+    location varchar(255),
+    working_hours varchar(255),
+    classification varchar(255),
+	salary varchar(255)
+);
