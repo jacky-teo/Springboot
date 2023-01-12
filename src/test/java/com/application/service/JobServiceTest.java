66af36bd609353b28541e634046d985a82c9a599
jacky-teo <jackytjq@outlook.com>
2023-01-12 13:05:17 +0800

    Change test cases

--- src/test/java/com/application/service/JobServiceTest.java
+++ src/test/java/com/application/service/JobServiceTest.java
@@ -6,6 +6,7 @@ import com.application.exception.NoSuchJobException;
 import com.application.model.Job;
 import com.application.repo.JobRepo;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -23,8 +24,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -38,100 +38,104 @@ class JobServiceTest {
 
     @BeforeEach
     void setUp(){
-        String[] firstJobDescription = {"Basic Salary - $2K + Commission", "Replacement leave for work on weekends", "Overseas Allowance", "Flight and Accommodations paid for overseas travels`"};
-        String[] firstJobResponsibilities = {"Build value based customer relationships","Develop a strategic networking platform with clients"};
-        String[] firstJobRequirements = {"Possess Degree","Excellent Spoken and Written English"};
-        job = new Job(1L,
-                "sales engineer",
-                firstJobDescription,
-                firstJobResponsibilities,
-                firstJobRequirements,
-                "Engineer",
-                "Central",
-                "9 Hours",
-                "Finance",
-                "$1000 - $20000");
+        job = mock(Job.class);
     }
 
     @Test
     void NoJobsListed(){
+        System.out.println("Running test no job listed");
         //Given setup precondition
         given(jopRepo.findAll()).willReturn(List.of());
         assertThrows(NoJobsListedException.class,()->{
             jobService.getAllJobs();
         });
+        System.out.println("Test no job listed success");
     }
 
     @Test // Get all jobs currently
     void getAllJobs() {
-        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
-        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
-        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+        System.out.println("Running test get all jobs ");
 
-        Job job2 = new Job(2L,
-                "Product Manager",
-                secondJobDescription,
-                secondJobResponsibilities,
-                secondJobRequirements,
-                "Product",
-                "North",
-                "9 Hours",
-                "Finance",
-                "$1000 - $3000");
-
+        Job job2 = mock(Job.class);
         //Given setup precondition
         given(jopRepo.findAll()).willReturn(List.of(job,job2));
-
         //When -- Action being tested
         List<Job> jobList = jobService.getAllJobs();
-        System.out.println((jobList));
+
         assertNotNull(jobList);
         assertEquals(2,jobList.size());
 
+        System.out.println("Test get all jobs success");
     }
 
     @Test
     void getJobById() {
+        System.out.println("Running test get job by ID ");
+
         //given
         given(jopRepo.findById(1L)).willReturn(Optional.of(job));
         //when
-        Job savedJob = jobService.getJobById(job.getId());
+        Job savedJob = jobService.getJobById(1L);
         //then
         System.out.println((savedJob));
         assertNotNull(savedJob);
+
+        System.out.println("Test get job by ID Success");
     }
 
     @Test
     void NoJobByIdFound(){
+        System.out.println("Running test no job id found ");
         given(jopRepo.findById(2L)).willReturn(Optional.empty());
         //When
         assertThrows(NoSuchJobException.class,()->{
             jobService.getJobById(2L);
         });
+        System.out.println("Test get no job by ID Success");
     }
 
     @Test
-    void createJob() {
-        //given
-        given(jopRepo.save(job)).willReturn(job);
-        System.out.println(jopRepo);
-        System.out.println(jobService);
-
-        //when
-        Job savedJob = jobService.createJob(job);
-
-        System.out.println(savedJob);
-        //then
-        assertNotNull(savedJob);
-    }
-
-    @Test // Empty Job Title upon creation
-    void createJobFailed(){
+    void testCreateJob() {
+        System.out.println("Running test create job");
+        System.out.println("Running create job failed test due to empty job title");
 
         String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
         String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
         String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
-        Job job2 = Job.builder()
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        //given
+        given(jopRepo.save(jobToSave)).willReturn(jobToSave);
+
+        //when
+        Job savedJob = jobService.createJob(jobToSave);
+
+        System.out.println(savedJob);
+        //then
+        assertNotNull(savedJob);
+        System.out.println("Test Create Job Success");
+    }
+
+    @Test // Empty Job Title upon creation
+    void testCreateJobFailedEmptyJobTitle(){
+        System.out.println("Running create job failed test due to empty job title");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
                 .job_title("")
                 .job_type("Product")
                 .job_description(secondJobDescription)
@@ -143,17 +147,234 @@ class JobServiceTest {
                 .classification("Finance")
                 .build();
 
-
-        //
-        given(jopRepo.save(job2)).willReturn(job2);
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
 
         //When
         assertThrows(CreateJobException.class,()->{
-            jobService.createJob(job2);
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty job title Success");
+    }
+
+    @Test // Empty Job Description upon creation
+    void testCreateJobFailedEmptyJobDescription(){
+        System.out.println("Running create job failed test due to empty job description");
+
+        String[] secondJobDescription = {};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
         });
 
         verify(jopRepo,never()).save(any(Job.class ));
 
+        System.out.println("Test create job failed test due to empty job description success");
+    }
+
+    @Test // Empty Job Description upon creation
+    void testCreateJobFailedEmptyJobResponsibilities(){
+        System.out.println("Running create job failed test due to empty job responsibilities");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+
+        System.out.println("Test create job failed test due to empty job responsibilities success");
+    }
+
+    @Test // Empty Job Requirements upon creation
+    void testCreateJobFailedEmptyJobRequirements(){
+        System.out.println("Running create job failed test due to empty requirements");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty job requirements Success");
+    }
+
+    @Test // Empty Job Title upon creation
+    void testCreateJobFailedEmptySalary(){
+        System.out.println("Running create job failed test due to empty job title");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty salary Success");
+    }
+
+    @Test // Empty Job working hours upon creation
+    void testCreateJobFailedEmptyWorkingHours(){
+        System.out.println("Running create job failed test due to empty working hours");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("")
+                .location("North")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty working hours Success");
+    }
+
+    @Test // Empty Location upon creation
+    void testCreateJobFailedEmptyLocation(){
+        System.out.println("Running create job failed test due to empty location");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("Engineer")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("")
+                .classification("Finance")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty location");
+    }
+
+    @Test // Empty Classification upon creation
+    void testCreateJobFailedEmptyClassification(){
+        System.out.println("Running create job failed test due to empty classification");
+
+        String[] secondJobDescription = {"Basic Salary - $1K + Commission", "4 day work week", "Local Allowance"};
+        String[] secondJobResponsibilities = {"Build value based products","Develop a relationships"};
+        String[] secondJobRequirements = {"Possess Diploma","Excellent Spoken and Written Chinese"};
+
+        Job jobToSave = Job.builder()
+                .job_title("")
+                .job_type("Product")
+                .job_description(secondJobDescription)
+                .job_responsibility(secondJobResponsibilities)
+                .requirements(secondJobRequirements)
+                .salary("$1000 - $3000")
+                .working_hours("9 Hours")
+                .location("North")
+                .classification("")
+                .build();
+
+        given(jopRepo.save(any(Job.class))).willReturn(jobToSave);
+
+        //When
+        assertThrows(CreateJobException.class,()->{
+            jobService.createJob(jobToSave);
+        });
+
+        verify(jopRepo,never()).save(any(Job.class ));
+        System.out.println("Test create job failed test due to empty classification");
     }
 
     @Test
@@ -186,6 +407,7 @@ class JobServiceTest {
     }
 
     @Test
+    @Disabled
     void uploadFile() {
 
     }
